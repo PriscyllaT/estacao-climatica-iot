2b9de3dd8f910491ccbd32b4441ecce9ff57b4cf
Priscylla-SSD-2\Priscylla-SSD-2016 <priscyllatavares@gmail.com>
2018-04-24 22:02:33 -0300

    Refatorando codigo e traduzindo

--- mqtt_adafruit_thingspeak/mqtt_adafruit_thingspeak.ino
+++ mqtt_adafruit_thingspeak/mqtt_adafruit_thingspeak.ino
@@ -26,7 +26,6 @@ int status = WL_IDLE_STATUS;
 SoftwareSerial Serial1(3, 4);
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiEspClient client;
-
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, SERVER, SERVERPORT, USERNAME, MQTT_KEY);
 
@@ -36,25 +35,31 @@ const char * myWriteAPIKey = "FTXKGNZ7M6UIT8V9";
 // Notice MQTT paths follow the form: channels/<channelId>/publish/fields/field<fieldNumer>
 Adafruit_MQTT_Publish temperatura = Adafruit_MQTT_Publish(&mqtt, "channels/452132/publish/fields/field1/FTXKGNZ7M6UIT8V9");
 Adafruit_MQTT_Publish umidade = Adafruit_MQTT_Publish(&mqtt, "channels/452132/publish/fields/field2/FTXKGNZ7M6UIT8V9");
-
+Adafruit_MQTT_Publish chuva = Adafruit_MQTT_Publish(&mqtt, "channels/452132/publish/fields/field3/FTXKGNZ7M6UIT8V9");
+int chuvaDigital = 6; //Pino ligado ao D0 do sensor
+int chuvaAnalogico = A0; //Pino ligado ao A0 do sensor
+int chuvaDigitalValor = 0
+int chuvaAnalogicoValor = 0
 /*************************** Sketch Code ************************************/
 
 void MQTT_connect();
 
 void setup() {
+  pinMode(chuvaDigital, INPUT);
+  pinMode(chuvaAnalogico, INPUT);
   Serial.begin(9600);
   delay(10);
-dht.begin();
+  dht.begin();
   Serial.println(F("MQTT - Estacao Climatica IoT"));
   Serial1.begin(19200);
 
   WiFi.init(&Serial1);
-  // Connect to WiFi access point.
+  //Connect to WiFi access point.
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
-// attempt to connect to WiFi network
+  //attempt to connect to WiFi network
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(WLAN_SSID);
@@ -74,9 +79,9 @@ void loop() {
   // Ensure the connection to the MQTT server is alive (this will make the first
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
   // function definition further below.
-
+  
     MQTT_connect();
-     
+    
     float t = dht.readTemperature(); // Read temperature from DHT sensor.
     float h = dht.readHumidity();  // Read humidity from DHT sensor
    
@@ -95,13 +100,34 @@ void loop() {
     Serial.print(F("\nEnviando umidade "));
     Serial.print(h);
   
-     if (! umidade.publish(h)) {
+    if (! umidade.publish(h)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+
+    //Le e arnazena o valor do pino digital
+    chuvaDigitalValor  = digitalRead(chuvaDigital);
+    //Le e armazena o valor do pino analogico
+    chuvaAnalogicoValor = analogRead(chuvaAnalogico);
+    //Envia as informacoes para o serial monitor
+    Serial.print("Valor digital : ");
+    Serial.print(val_d);
+    Serial.print(" - Valor analogico : ");
+    Serial.println(val_a);
+
+    delay15s();
+    // Now we can publish stuff!
+    Serial.print(F("\nEnviando temperatura "));
+    Serial.print(chuvaDigitalValor);
+    Serial.print("...");
+    if (! chuva.publish(chuvaDigitalValor)) {
       Serial.println(F("Failed"));
     } else {
       Serial.println(F("OK!"));
     }
  
-  delay30s();
+    delay30s();
  
 }
 
